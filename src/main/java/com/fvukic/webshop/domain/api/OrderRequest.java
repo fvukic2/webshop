2567499ac2407758b10dba824abecf452698dda8
Filip Vukic <filip.vukic@trilix.eu>
2023-05-30 23:24:27 +0200

    Added code to calculate total price of order

--- src/main/java/com/fvukic/webshop/domain/api/OrderRequest.java
+++ src/main/java/com/fvukic/webshop/domain/api/OrderRequest.java
@@ -2,7 +2,6 @@ package com.fvukic.webshop.domain.api;
 
 import com.fvukic.webshop.domain.entity.Article;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +16,8 @@ public class OrderRequest {
     @NotBlank
     private String description;
 
-    @NotNull
-    private Double totalPrice;
+//    @NotNull
+//    private Double totalPrice;
 
     private List<Article> articles;
 }
