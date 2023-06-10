3d3fb7dba3c25eeaf6a24303d066ee2ffd80e405
fvukic <filip.vukic55@gmail.com>
2023-06-10 15:12:33 +0200

    Added city entity,request,service and repository

---
+++ src/main/java/com/fvukic/webshop/domain/entity/City.java
@@ -0,0 +1,26 @@
+package com.fvukic.webshop.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "city")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cityId")
+    private Integer cityId;
+
+    @Column(name = "city")
+    private String city;
+
+}
---
+++ src/main/java/com/fvukic/webshop/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package com.fvukic.webshop.repository;
+
+import com.fvukic.webshop.domain.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City,Integer> {
+}
