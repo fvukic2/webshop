b581a0aa8a996a2c019bbca2573e95c1c5515e26
fvukic <filip.vukic55@gmail.com>
2023-06-30 14:18:52 +0200

    Small changes

--- src/main/java/com/fvukic/webshop/role/Role.java
+++ src/main/java/com/fvukic/webshop/role/Role.java
@@ -20,7 +20,7 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id")
-    private Integer role_id;
+    private Integer roleId;
 
     @Column(name = "role_name")
     private String roleName;
