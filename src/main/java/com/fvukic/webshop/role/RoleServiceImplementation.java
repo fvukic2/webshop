1f4d45ea3be4fd11555fc9360fdbbf027a6ef56f
fvukic <filip.vukic55@gmail.com>
2023-07-06 20:49:49 +0200

    Added logging for Role and UserServiceImplementation

--- src/main/java/com/fvukic/webshop/role/RoleServiceImplementation.java
+++ src/main/java/com/fvukic/webshop/role/RoleServiceImplementation.java
@@ -3,6 +3,8 @@ package com.fvukic.webshop.role;
 import com.fvukic.webshop.exception.EntityWithIdNotFoundException;
 import com.fvukic.webshop.role.model.RoleRequest;
 import com.fvukic.webshop.util.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,39 +14,57 @@ public class RoleServiceImplementation implements RoleService {
 
     private RoleRepository roleRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(RoleServiceImplementation.class);
+
     public RoleServiceImplementation(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
 
     @Override
     public List<Role> getAllRoles() {
-        return roleRepository.findAll();
+        logger.info("Fetching all roles");
+        List<Role> roles = roleRepository.findAll();
+        logger.info("Fetched {} roles", roles.size());
+        return roles;
     }
 
     @Override
     public void saveNewRoleRequest(RoleRequest roleRequest) {
+        logger.info("Saving new role request");
         Role role = getRoleRequest(roleRequest);
         roleRepository.save(role);
+        logger.info("Saved new role request");
     }
 
     @Override
     public void updateRoleRequest(RoleRequest roleRequest, Integer roleId) {
+        logger.info("Updating role with ID: {}", roleId);
         Role existingRole = roleRepository.findById(roleId)
-                .orElseThrow(() -> new EntityWithIdNotFoundException(ErrorResponse.ERROR_WRONG_ID,roleId));
+                .orElseThrow(() -> {
+                    logger.error("Error occurred while updating role: Role with ID {} not found", roleId);
+                    return new EntityWithIdNotFoundException(ErrorResponse.ERROR_WRONG_ID,roleId);
+                });
         existingRole.setRoleName(roleRequest.getRoleName());
         roleRepository.save(existingRole);
+        logger.info("Updated role with ID: {}", roleId);
     }
 
     @Override
     public void deleteRoleRequest(Integer roleId) {
+        logger.info("Deleting role with ID: {}", roleId);
         if (!roleRepository.existsById(roleId)){
+            logger.error("Error occurred while deleting role: Role with ID {} not found", roleId);
             throw new EntityWithIdNotFoundException(ErrorResponse.ERROR_WRONG_ID,roleId);
         }
         roleRepository.deleteById(roleId);
+        logger.info("Deleted role with ID: {}", roleId);
     }
 
     private Role getRoleRequest(RoleRequest roleRequest){
-        return Role.builder().roleName(roleRequest.getRoleName()).build();
+        logger.info("Getting role from request");
+        Role role = Role.builder().roleName(roleRequest.getRoleName()).build();
+        logger.info("Got role from request");
+        return role;
 
     }
 }
--- src/main/java/com/fvukic/webshop/user/UserServiceImplementation.java
+++ src/main/java/com/fvukic/webshop/user/UserServiceImplementation.java
@@ -3,6 +3,8 @@ package com.fvukic.webshop.user;
 import com.fvukic.webshop.exception.EntityWithIdNotFoundException;
 import com.fvukic.webshop.user.model.UserRequest;
 import com.fvukic.webshop.util.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,42 +14,60 @@ public class UserServiceImplementation implements UserService {
 
     private UserRepository userRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImplementation.class);
+
     public UserServiceImplementation(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        logger.info("Fetching all users");
+        List<User> users = userRepository.findAll();
+        logger.info("Fetched {} users", users.size());
+        return users;
     }
 
     @Override
     public void saveNewUserRequest(UserRequest userRequest) {
+        logger.info("Saving new user request");
         User user = getUserRequest(userRequest);
         userRepository.save(user);
+        logger.info("Saved new user request");
     }
 
     @Override
     public void updateUserRequest(UserRequest userRequest, Integer userId) {
+        logger.info("Updating user with ID: {}", userId);
         User existingUser = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityWithIdNotFoundException(ErrorResponse.ERROR_WRONG_ID,userId));
+                .orElseThrow(() -> {
+                    logger.error("Error occurred while updating user: User with ID {} not found", userId);
+                    return new EntityWithIdNotFoundException(ErrorResponse.ERROR_WRONG_ID,userId);
+                });
         existingUser.setUsername(userRequest.getUsername());
         existingUser.setPassword(userRequest.getPassword());
         existingUser.setRole(userRequest.getRole());
         userRepository.save(existingUser);
+        logger.info("Updated user with ID: {}", userId);
     }
 
     @Override
     public void deleteUserRequest(Integer userId) {
+        logger.info("Deleting user with ID: {}", userId);
         if (!userRepository.existsById(userId)){
+            logger.error("Error occurred while deleting user: User with ID {} not found", userId);
             throw new EntityWithIdNotFoundException(ErrorResponse.ERROR_WRONG_ID,userId);
         }
         userRepository.deleteById(userId);
+        logger.info("Deleted user with ID: {}", userId);
     }
 
     private User getUserRequest(UserRequest userRequest){
-        return User.builder().username(userRequest.getUsername())
+        logger.info("Getting user from request");
+        User user = User.builder().username(userRequest.getUsername())
                 .role(userRequest.getRole())
                 .password(userRequest.getPassword()).build();
+        logger.info("Got user from request");
+        return user;
     }
 }
